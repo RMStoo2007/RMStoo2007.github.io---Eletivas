47d01f059a28a23aa6a1dbef0a5cbc530948c232
RMStoo2007 <94657022+RMStoo2007@users.noreply.github.com>
2021-11-19 14:03:43 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,11 @@
+function zoom (image)
+{
+    //image.className = "content_Imagens100";
+    image.style = "width: 80%";
+}
+
+function unzoom (image)
+{
+    //image.className = "content_Imagens100";
+    image.style = "width: 50%";
+}
